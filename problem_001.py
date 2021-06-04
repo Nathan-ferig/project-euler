cfe6639f13959a714f9a840d5b647fbab71362db
Nathan Ferig <nathan.ferig@gmail.com>
2021-06-03 21:03:28 -0300

    Generalização concluida

--- problem_001.py
+++ problem_001.py
@@ -1,9 +1,49 @@
+"""
+Solution for Project Euler problem 001
+https://projecteuler.net/about
+
+Problem 001 asks for the sum of all the multiples of 3 or 5 bellow 1000. 
+
+Two solutions are given.
+Solutions were generalized to find the sum of all multiples of two given integers bellow given integer
+"""
+
+
 def sum_by_trial(n: int = 1000, a: int=3, b: int=5) -> int:
+    """This solution tests all integers bellow n and check if they are multiples of a or b"""
     return sum(i for i in range(n) if (i % a ==0 or i % b == 0))
 
 def sum_by_math(n: int = 1000, a: int=3, b: int=5) -> int:
-    c = a * b
-    return int((a/2)*(int((n-1)/a))*(int((n+a-1)/a)) + (b/2)*(int((n-1)/b))*(int((n+b-1)/b)) - (c/2)*(int((n-1)/c))*(int((n+c-1)/c)))
+    """This solution uses arithmetic progression the find the sum of a series:
+    t1 is the sum of all multiples of a bellow n
+    t2 is the sum of all multiples of b bellow n
+    t3 is the sum of all commom numbers from t1 and t2
+    thus the solution is t1 + t2 - t3
+    """
+    c = least_common_multiple(a,b)
+    t1 = int((a/2)*(int((n-1)/a))*(int((n+a-1)/a)))
+    t2 = int((b/2)*(int((n-1)/b))*(int((n+b-1)/b)))
+    t3 = int((c/2)*(int((n-1)/c))*(int((n+c-1)/c)))
+    return t1 + t2 - t3
 
-print(sum_by_trial(1000,2,7))
-print(sum_by_math(1000,2,7))
+def least_common_multiple(a,b):
+    c = greatest_common_factor(a,b)
+    return (a*b)/c
+
+def greatest_common_factor(a,b):
+    n = 0
+    if a > b:
+        while b != 0:
+            n = a % b
+            a = b
+            b = n
+        return a
+    else:
+        while a != 0:
+            n = b % a
+            b = a
+            a = n
+        return b
+
+print(sum_by_trial(1000,2,21))
+print(sum_by_math(1000,2,21))
