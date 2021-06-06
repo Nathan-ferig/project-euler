a2224ccd7db2e1a3e968a64f0b83b9f778d3046c
Nathan Ferig <nathan.ferig@gmail.com>
2021-06-06 18:28:52 -0300

    Problem 004

---
+++ problem_004.py
@@ -0,0 +1,20 @@
+"""
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+a  = 102
+b = str(a)
+print(b)
+print(b[::-1])
+"""
+
+def largest_palindrome():
+    palindrome = []
+    for i in range(999,99,-1):
+        for j in range(i,99,-1):
+            mult = i*j
+            if (str(mult) == str(mult)[ : : -1]):
+                palindrome.append(mult)
+    return max(palindrome)
+
+print(largest_palindrome())
