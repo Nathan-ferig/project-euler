08e922fb03a3c84601ce45dc948549d048d959f3
Nathan Ferig <nathan.ferig@gmail.com>
2021-06-08 18:00:52 -0300

    Added solution for problem 6

--- problem_006.py
+++ problem_006.py
@@ -1,14 +1,21 @@
 """
-Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+Solution for Project Euler problem 006
+https://projecteuler.net/about
+
+Problem 006 asks for the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+Solution were generalized to find the answer for any range of natural numbers. 
 """
 
 def Sum_Of_Squares(max_number: int = 10, min_number: int = 1) -> int:
+    """Computes the sum of the squares"""
     sum: int = 0
     for i in range(min_number,max_number+1):
         sum += i*i
     return sum
 
 def Square_Of_Sum(max_number: int = 10, min_number: int = 1) -> int:
+    """Computes the square of the sum"""
     sum: int = 0
     for i in range(min_number,max_number+1):
         sum += i
@@ -16,6 +23,8 @@ def Square_Of_Sum(max_number: int = 10, min_number: int = 1) -> int:
     return sum
 
 def Difference(max_number: int = 10, min_number: int = 1) -> int:
+    """Computes the difference between the square of the sum
+    and the sum of the squares"""
     return Square_Of_Sum(max_number,min_number) - Sum_Of_Squares(max_number,min_number)
 
 print(Difference(100))
