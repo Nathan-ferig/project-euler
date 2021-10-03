3f80b773f492d681be20fd95437d3fd2d7873af4
Nathan Ferig <nathan.ferig@gmail.com>
2021-10-03 12:49:29 -0300

    Problem 9 solved with comments

--- problem_009.py
+++ problem_009.py
@@ -1,17 +1,26 @@
+"""
+Solution for Project Euler problem 009
+https://projecteuler.net/about
+
+Problem 009 asks for the product of the Pythagorean triplet's terms whose sum equals 1000.
+
+Solution uses a direct approach, testing all possible combinations of a and b to find the missing c.
+"""
+
 def Special_triplet():
-    for a in range(1,1000):
-        for b in range(a+1,1000):
+    for a in range(1,500):
+        for b in range(a+1,500):
             c_square = a**2 + b**2
             c = c_square**.5
             if c.is_integer():
-                soma = a + b + int(c)
-                if soma == 1000:
-                    return(a,b,c,soma)
+                sum = a + b + int(c)
+                if sum == 1000:
+                    return(a,b,c,sum)
     return("Special triplet not found!")
 
-a,b,c,soma = Special_triplet()
+a,b,c,sum = Special_triplet()
 print(a)
 print(b)
 print(c)
-print(soma)
-print('Produto de a*b*c: ', a*b*c)
+print(sum)
+print('Product of a*b*c: ', a*b*c)
